88ac60fe0d33ed63be1eac376753ec10144bbea0
edyson10 <edysonfabian@hotmail.com>
2020-10-24 14:22:11 -0500

    Se actualiza la conexion al servidor AWS

--- conexion.php
+++ conexion.php
@@ -1,16 +1,15 @@
 <?php
-
+/*
 $db = [
     'host' => 'localhost',
     'username' => 'root',
     'password' => '',
     'db' => 'carshop' //Cambiar al nombre de tu base de datos
 ];
-/*
+*/
 $db = [
     'host' => '54.144.85.160',
     'username' => 'admin',
     'password' => 'ambu12345',
-    'db' => 'ambu'
+    'db' => 'carshop'
 ];
-*/
