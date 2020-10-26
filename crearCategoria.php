1439b4c2ced10bb1843264dc93f008ead430225c
edyson10 <edysonfabian@hotmail.com>
2020-10-26 01:59:05 -0500

    Se modifico el servicio de crear categoria

--- crearCategoria.php
+++ crearCategoria.php
@@ -7,8 +7,8 @@ $dbConn =  connect($db);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $input = $_POST;
-	$sql = "INSERT INTO categoria (id_categoria, nombre, capa_bateria, carg_util, capa_espacio) 
-            VALUES (NULL, :nombre, NULL, NULL, NULL);";
+	$sql = "INSERT INTO categoria (id_categoria, nombre, tipo, cantidad) 
+            VALUES (NULL, :nombre, NULL, NULL);";
 	$statement = $dbConn->prepare($sql);
 	bindAllValues($statement, $input);
     $statement->execute();
